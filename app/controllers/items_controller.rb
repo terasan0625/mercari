7c2792304cc7706eb6d3ebfa044af4f57de0b1b5
EC2 Default User <ec2-user@ip-172-31-30-79.ap-northeast-1.compute.internal>
2024-01-11 05:48:06 +0000

    20240111

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,13 +2,26 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-    # @item = Item.new(item_params)
-
-    # @item.save
-    # redirect_to item_path(@item.id)
-  
+    
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item.id)
+  end
 
   def show
+    item = Item.find(params[:id])
+    if item.weight == 25
+     @result = 25 
+    elsif item.weight == 50
+     @result = 50
+    elsif item.weight == 100
+     @result = 100
+    elsif item.weight == 150
+     @result = 150
+    elsif item.weight == 200
+     @result = 200
+    end
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources:items, only:[:show,:new]
+  resources:items, only:[:show,:create,:new]
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
